17c1055a99d151cec7ea6aa1c4f103bd7a4b67b1
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-06-15 10:35:56 -0500

    Se creo validacion a nivel de clase

--- ExampleApp/Models/Product.cs
+++ ExampleApp/Models/Product.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Http;
 
 namespace ExampleApp.Models
 {
-    public class Product
+    public class Product : IValidatableObject
     {
         public int ProductID { get; set; }
         public string Name { get; set; }
@@ -14,5 +16,31 @@ namespace ExampleApp.Models
 
         [HttpBindNever]
         public bool IncludeInSale { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> errors = new List<ValidationResult>();
+
+            if (Name == null || Name == string.Empty)
+            {
+                errors.Add(new ValidationResult("A value is required for the Name property"));
+            }
+
+            if (Price == 0)
+            {
+                errors.Add(new ValidationResult("A value is required for the Price property"));
+            }
+            else if (Price < 1 || Price > 2000)
+            {
+                errors.Add(new ValidationResult("The price value is out of range"));
+            }
+
+            if (IncludeInSale)
+            {
+                errors.Add(new ValidationResult("Request cannot contain values for IncludeInSale"));
+            }
+
+            return errors;
+        }
     }
 }
