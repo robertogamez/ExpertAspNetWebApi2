59287680247cc2e12e57a6e380910c7256119a41
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-06-26 17:07:25 -0500

    Se añade validaciones para rutas

--- Dispatch/App_Start/WebApiConfig.cs
+++ Dispatch/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
+using System.Web.Http.Routing.Constraints;
 
 namespace Dispatch
 {
@@ -16,10 +17,40 @@ namespace Dispatch
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            //config.Routes.MapHttpRoute(
+            //    name: "ChromeRoute",
+            //    routeTemplate: "api/today/DayOfWeek",
+            //    defaults: new { controller = "today", action = "dayofweek" },
+            //    constraints: new { useragent = new UserAgentConstraint("chrome") }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "NotChromeRoute",
+            //    routeTemplate: "api/today/DayOfWeek",
+            //    defaults: new { controller = "today", action = "daynumber" }
+            //);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "IncludeAction",
+            //    routeTemplate: "api/today/{action}/{day}",
+            //    defaults: new { controller = "today", day = 6 }
+            //);
+
             config.Routes.MapHttpRoute(
-                name: "IncludeAction",
-                routeTemplate: "api/today/{action}/{day}",
-                defaults: new { controller = "today", day = 6 }
+                name: "ChromeRoute",
+                routeTemplate: "api/today/{action}",
+                defaults: new { controller = "today" },
+                constraints: new
+                {
+                    useragent = new UserAgentConstraint("Chrome"),
+                    action = new RegexRouteConstraint("daynumber|othermethod")
+                }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "NotChromeRoute",
+                routeTemplate: "api/today/DayOfWeek",
+                defaults: new { controller = "today", action = "daynumber" }
             );
 
             config.Routes.MapHttpRoute(
