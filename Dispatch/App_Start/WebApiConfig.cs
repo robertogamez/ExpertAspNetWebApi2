881a6412bb6daa5a09edfee78332a6ebc7e36cd7
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-07-02 16:23:51 -0500

    Se prepara el proyecto de ejemplo

--- Dispatch/App_Start/WebApiConfig.cs
+++ Dispatch/App_Start/WebApiConfig.cs
@@ -18,6 +18,12 @@ namespace Dispatch
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
+                name: "ActionMethods",
+                routeTemplate: "api/{controller}/{action}/{day}",
+                defaults: new { day = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
--- Dispatch/Controllers/TodayController.cs
+++ Dispatch/Controllers/TodayController.cs
@@ -10,15 +10,13 @@ namespace Dispatch.Controllers
     [RoutePrefix("api/today")]
     public class TodayController : ApiController
     {
-        //[HttpGet]
-        //[Route("dayofweek")]
-        //public string DayOfWeek()
-        //{
-        //    return DateTime.Now.ToString();
-        //}
+        [HttpGet]
+        public string DayOfWeek()
+        {
+            return DateTime.Now.ToString("dddd");
+        }
 
         [HttpGet]
-        [Route("dayofweek/{day:int=-1}")]
         public string DayNumber(int day)
         {
             if (day != -1)
@@ -32,7 +30,6 @@ namespace Dispatch.Controllers
         }
 
         [HttpGet]
-        [Route("~/getdaynumber")]
         public string DayOfWeek(int day)
         {
             return Enum.GetValues(typeof(DayOfWeek)).GetValue(day).ToString();
