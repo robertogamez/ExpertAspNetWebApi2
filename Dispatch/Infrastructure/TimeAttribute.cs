da0fc1e36a806ec2ff003b75d80c7a4d4944f893
Roberto Manuel Balderas Gamez <vn00m8d@mx.wal-mart.com>
2018-07-03 17:22:16 -0500

    Se crea primer Action Filter

---
+++ Dispatch/Infrastructure/TimeAttribute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Dispatch.Infrastructure
+{
+    public class TimeAttribute : Attribute, IActionFilter
+    {
+        public bool AllowMultiple
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(
+            HttpActionContext actionContext, CancellationToken cancellationToken, 
+            Func<Task<HttpResponseMessage>> continuation)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            HttpResponseMessage result = await continuation();
+
+            long elapsedTicks = sw.ElapsedTicks;
+
+            result.Headers.Add("Elapsed-Time", elapsedTicks.ToString());
+            Debug.WriteLine("Elapsed time: {0} ticks, {1} {2}", elapsedTicks, actionContext.Request.Method, actionContext.Request.RequestUri);
+            return result;
+        }
+    }
+}
